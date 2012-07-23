f116f6b4f4c42084096d28507699c9da9cb9fbe7
足兆叉虫 <17175297.hk@gmail.com>
2012-07-23 20:38:34 +0800

    bug fix

--- ThunderLixianExporter.js
+++ ThunderLixianExporter.js
@@ -282,7 +282,7 @@ TLE.exporter = {
   
   TLE.url_rewrite = function(url, filename) {
     url = url.replace("vip.xunlei.com/download", "vip.xunlei.com/"+encodeURIComponent(filename));
-    url = url.replace(/n=\w+/, "n=0");
+    url = url.replace(/&n=\w+/, "&n=0");
     return url;
   };
 
