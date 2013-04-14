aad6b5b2dcca95aaad635b9031adb67a3b93f4ef
Binux <17175297.hk@gmail.com>
2013-04-14 19:45:38 +0800

    update version

--- ThunderLixianExporter.meta.js
+++ ThunderLixianExporter.meta.js
@@ -1,12 +1,13 @@
 // ==UserScript==
 // @name       ThunderLixianExporter
 // @namespace  http://dynamic.cloud.vip.xunlei.com/
-// @version    0.42
+// @version    0.5
 // @description  export thunder lixian url to aria2/wget
-// @match      http://dynamic.cloud.vip.xunlei.com/user_task?*
-// @match      http://61.147.76.6/iplay.html?*
-// @match      http://222.141.53.5/iplay.html?*
+// @match      http://dynamic.cloud.vip.xunlei.com/
+// @match      http://61.147.76.6/iplay.html
+// @match      http://222.141.53.5/iplay.html
 // @run-at document-end
 // @copyright  2012+, Binux <root@binux.me>
 // @updateURL http://s.binux.me/TLE/master/ThunderLixianExporter.meta.js
 // ==/UserScript==
+
