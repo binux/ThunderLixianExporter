1465148dfb2dfd97ebd0346cc3dec14343b78f51
Binux <17175297.hk@gmail.com>
2013-04-14 19:49:14 +0800

    match bug fix

--- ThunderLixianExporter.meta.js
+++ ThunderLixianExporter.meta.js
@@ -3,9 +3,9 @@
 // @namespace  http://dynamic.cloud.vip.xunlei.com/
 // @version    0.5
 // @description  export thunder lixian url to aria2/wget
-// @match      http://dynamic.cloud.vip.xunlei.com/
-// @match      http://61.147.76.6/iplay.html
-// @match      http://222.141.53.5/iplay.html
+// @match      http://dynamic.cloud.vip.xunlei.com/*
+// @match      http://61.147.76.6/iplay.html*
+// @match      http://222.141.53.5/iplay.html*
 // @run-at document-end
 // @copyright  2012+, Binux <root@binux.me>
 // @updateURL http://s.binux.me/TLE/master/ThunderLixianExporter.meta.js
